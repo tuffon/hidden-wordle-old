1273ba1d035140a27e351e699a7b9176c84bad51
thornm9 <tm.jalal@gmail.com>
2022-03-10 17:25:32 -0800

    finish api endpoint

---
+++ api/_mongodb_client.ts
@@ -0,0 +1,24 @@
+"use strict"
+// Import the dependency.
+import { MongoClient, MongoClientOptions } from "mongodb"
+const uri =
+  "mongodb+srv://application:Lmabkn067AP@cluster0.xwl5x.mongodb.net/wordle_extreme?retryWrites=true&w=majority"
+const options: MongoClientOptions = {}
+let client
+let clientPromise
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (hot module replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
+}
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise
