318f94a2e3a15b77ce26fdf5b5bf1b3ebeb8febe
Tuffon Benavides <benavides.tuffon@gmail.com>
2022-03-14 12:24:04 -0700

    added debug

--- api/_mongodb_client.ts
+++ api/_mongodb_client.ts
@@ -15,6 +15,7 @@ if (process.env.NODE_ENV === "development") {
   clientPromise = global._mongoClientPromise
 } else {
   // In production mode, it's best to not use a global variable.
+  console.debug(uri)
   client = new MongoClient(uri, options)
   clientPromise = client.connect()
 }
